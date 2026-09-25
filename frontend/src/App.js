70a8b69104c077ad0d6862366331603469f2f96d
didarmadikenov8-creator <didarmadikenov8@gmail.com>
2026-09-26 00:45:55 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ const Home = () => {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/evgeniy-osteopat">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
